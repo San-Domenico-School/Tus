91eb45306c662848e79831cc9c2d8a44ab5aaa64
TF2026 <tfleitas26@sandomenico.org>
2025-03-14 09:31:24 -0700

    reece puzzle in scene and change scene working

--- Assets/Scripts/Utilities/Painter.cs
+++ Assets/Scripts/Utilities/Painter.cs
@@ -26,6 +26,7 @@ public class Painter : MonoBehaviour
     //public float paintRemaining { get; set; } = 50;
     public float paintRemaining = 500;
 
+
     private void Awake()
     {
         paintAction = new TusInputAction();
@@ -48,7 +49,7 @@ public class Painter : MonoBehaviour
     }
 
     private void Update()
-    { 
+    {
         HandlePainting();
     }
 
@@ -57,7 +58,6 @@ public class Painter : MonoBehaviour
     {
         return paintColor;
     }
-
     public void SetPaintColor(Color color)
     {
         paintColor = color;
@@ -81,7 +81,6 @@ public class Painter : MonoBehaviour
                 Debug.Log("PAINT RAN OUT!!!");
             }
         }
-
     }
 
     // Shoot a ray where the paint will be
@@ -99,8 +98,6 @@ public class Painter : MonoBehaviour
         Texture2D texture = ObjectStatisticsUtility.GetOrCreateObjectsTexture(hit.transform.gameObject, SaveLoadImagesManager.texelDensity);
 
         PaintTexture(hit.textureCoord, texture);
-
-        hit.transform.gameObject.GetComponent<PaintableObject>().lastPaintedColor = paintColor;
     }
 
     //paints the texture at the UV cordate with diameter of the brushSize and shape of brush 
