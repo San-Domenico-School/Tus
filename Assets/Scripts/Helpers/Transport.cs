a570eca6829d597e75f18aab99d7bcc00620e588
LoganR <112724859+Loganrud@users.noreply.github.com>
2025-05-09 12:41:49 -0700

    teleport might work

--- Assets/Scripts/Helpers/Transport.cs
+++ Assets/Scripts/Helpers/Transport.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Transport : MonoBehaviour
 {
     public Transform teleportDestination;
+    bool isReady = true;
+
+    IEnumerator IsReadySet(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        isReady = true;
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && isReady)
         {
+            isReady = false;
             other.transform.position = teleportDestination.position;
-            
+            StartCoroutine(IsReadySet(3f));
         }
     }
 }
